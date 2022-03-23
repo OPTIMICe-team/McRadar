820d6fe43144dc8df0a7b4a935ba0f96d7591ea4
Leonie von Terzi <lterzi@uni-koeln.de>
2022-03-23 14:25:59 +0100

    added SSRGA, Rayleigh

--- mcradar/radarOperator/spectraOperator.py
+++ mcradar/radarOperator/spectraOperator.py
@@ -2,7 +2,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 def getVelIntSpec(mcTable, mcTable_binned, variable):
     """
@@ -24,7 +25,7 @@ def getVelIntSpec(mcTable, mcTable_binned, variable):
     return mcTableVelIntegrated
 
 
-def getMultFrecSpec(wls, mcTable, velBins, velCenterBins , centerHeight):
+def getMultFrecSpec(wls, mcTable, velBins, velCenterBins , centerHeight, convolute,nave,noise_pow,eps_diss,uwind,time_int,theta,scatSet={'mode':'full', 'safeTmatrix':False}):
     """
     Calculation of the multi-frequency spectrograms 
     
@@ -49,15 +50,32 @@ def getMultFrecSpec(wls, mcTable, velBins, velCenterBins , centerHeight):
     for wl in wls:
     
         wlStr = '{:.2e}'.format(wl)
-	
-        mcTable['sZeMultH_{0}'.format(wlStr)] = mcTable['sZeH_{0}'.format(wlStr)] * mcTable['sMult']
-        mcTable['sZeMultV_{0}'.format(wlStr)] = mcTable['sZeV_{0}'.format(wlStr)] * mcTable['sMult']
+        
+        if (scatSet['mode'] == 'SSRGA') or (scatSet['mode'] == 'Rayleigh') or (scatSet['mode'] == 'SSRGA-Rayleigh'):
+          mcTable['sZeMultH_{0}'.format(wlStr)] = mcTable['sZeH_{0}'.format(wlStr)] * mcTable['sMult']
+          #print(mcTable['sMult'])
+          #plt.plot(mcTable['sZeMultH_{0}'.format(wlStr)],mcTable['radii_mm'],label='Mult')
+          #plt.plot(mcTable['sZeH_{0}'.format(wlStr)],mcTable['radii_mm'],label='sZe')
+          #plt.legend()
+          #plt.show()
+          intSpecH = getVelIntSpec(mcTable, mcTable_binned,'sZeMultH_{0}'.format(wlStr))
+          if convolute == True:
+              intSpecH = convoluteSpec(intSpecH,wl,velCenterBins,eps_diss,noise_pow,nave,theta,uwind,time_int,centerHeight)
+          tmpDataDic['spec_H_{0}'.format(wlStr)] = intSpecH.values[:,0]
+        
+        else:
+          mcTable['sZeMultH_{0}'.format(wlStr)] = mcTable['sZeH_{0}'.format(wlStr)] * mcTable['sMult']
+          mcTable['sZeMultV_{0}'.format(wlStr)] = mcTable['sZeV_{0}'.format(wlStr)] * mcTable['sMult']
 
-        intSpecH = getVelIntSpec(mcTable, mcTable_binned,'sZeMultH_{0}'.format(wlStr))
-        tmpDataDic['spec_H_{0}'.format(wlStr)] = intSpecH.values[:,0]
-
-        intSpecV = getVelIntSpec(mcTable, mcTable_binned, 'sZeMultV_{0}'.format(wlStr))
-        tmpDataDic['spec_V_{0}'.format(wlStr)] = intSpecV.values[:,0]
+          intSpecH = getVelIntSpec(mcTable, mcTable_binned,'sZeMultH_{0}'.format(wlStr))
+          intSpecV = getVelIntSpec(mcTable, mcTable_binned, 'sZeMultV_{0}'.format(wlStr))
+          if convolute == True:
+              intSpecH = convoluteSpec(intSpecH,wl,velCenterBins,eps_diss,noise_pow,nave,theta,uwind,time_int,centerHeight)
+              intSpecV = convoluteSpec(intSpecV,wl,velCenterBins,eps_diss,noise_pow,nave,theta,uwind,time_int,centerHeight)
+          tmpDataDic['spec_H_{0}'.format(wlStr)] = intSpecH.values[:,0]
+          tmpDataDic['spec_V_{0}'.format(wlStr)] = intSpecV.values[:,0]
+        
+        
 
     #converting to XR
     specTable = pd.DataFrame(data=tmpDataDic, index=velCenterBins)
@@ -66,3 +84,47 @@ def getMultFrecSpec(wls, mcTable, velBins, velCenterBins , centerHeight):
     specTable = specTable.rename_dims(dims_dict={'index':'vel'}).rename(name_dict={'index':'vel'})
     
     return specTable
+    
+def convoluteSpec(spec,wl,vel,eps,noise_pow,nave,theta,u_wind,time_avg,height):
+    """
+    this function convolutes the spectrum with turbulence and adds random noise, optional!
+    Parameters
+    ----------
+    spec: spectral data (pd.dataframe) [mm^6/m^3]
+    sigma_t: spectrum width due to turbulence
+    np: radar noise power [mm^6/m^3]
+    
+    Returns
+    -------
+    convoluted and noisy spectrum as pd.dataframe with the index of the input spec 
+    """
+
+    L_s = u_wind*time_avg + 2*height*np.sin(theta)
+    L_lam = wl/2    
+    sigma_t2 = 3/4*(eps/2*np.pi)^(2/3)*( L_s**(2/3) - L_lam**(2/3) )
+    
+    spec_turb = np.zeros(len(vel))#spec.copy()*np.NaN
+    dv = np.diff(vel)[0]
+    prefactor_turb = 1.0 / (np.sqrt(2.0 * np.pi) * np.sqrt(sigma_t2))
+    #- turbulence convolution:
+    for i in range(len(vel)):
+        integral = 0
+        for ii in range(len(vel)):
+            exp_arg = (-1.0*(vel[i]-vel[ii])**2)/(2.0*sigma_t2)
+            if exp_arg >= -100:
+                integral = integral + (spec.values[ii]*np.exp(exp_arg)*dv)
+        spec_turb[i] = prefactor_turb * integral
+    
+    #- add random noise
+    Ni = noise_pow / (len(vel) * dv)
+    
+    random_numbers = np.random.uniform(size=len(vel)*nave)
+    
+    S_bin_noise = np.zeros(len(vel))
+    
+    for iave in range(nave):
+        S_bin_noise = S_bin_noise + (-np.log(random_numbers[iave * (len(vel)) : ((iave+1) * len(vel))]) * (spec_turb + np.ones(len(vel))*Ni )) 
+    spectrum = S_bin_noise / nave
+    return pd.DataFrame(data=spectrum,index=spec.index)
+    
+    
