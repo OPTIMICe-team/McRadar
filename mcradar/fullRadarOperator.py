0a5c9e51c8ff17e7598862b9f159ea96175712e5
Leonie von Terzi <L.Terzi@lmu.de>
2023-04-04 17:46:53 +0200

    if mcTable is empty don't calculate

--- mcradar/fullRadarOperator.py
+++ mcradar/fullRadarOperator.py
@@ -8,53 +8,55 @@ from mcradar import *
 import matplotlib.pyplot as plt
 from mcradar.tableOperator import creatRadarCols
 def fullRadar(dicSettings, mcTable):
-    """
-    Calculates the radar variables over the entire range
+	"""
+	Calculates the radar variables over the entire range
 
-    Parameters
-    ----------
-    dicSettings: a dictionary with all settings output from loadSettings()
-    mcTable: McSnow data output from getMcSnowTable()
+	Parameters
+	----------
+	dicSettings: a dictionary with all settings output from loadSettings()
+	mcTable: McSnow data output from getMcSnowTable()
 
-    Returns
-    -------
-    specXR: xarray dataset with the spectra(range, vel) and KDP(range)
-    """
-
-
-    specXR = xr.Dataset()
-    #specXR_turb = xr.Dataset()
-    vol = dicSettings['gridBaseArea'] * dicSettings['heightRes']
-    mcTable = creatRadarCols(mcTable, dicSettings)
-    for i, heightEdge0 in enumerate(dicSettings['heightRange']):
-
-        heightEdge1 = heightEdge0 + dicSettings['heightRes']
-
-        print('Range: from {0} to {1}'.format(heightEdge0, heightEdge1))
-        mcTableTmp = mcTable.where((mcTable['sHeight']>heightEdge0) &
-                         (mcTable['sHeight']<=heightEdge1),drop=True)
-        #print(mcTableTmp)
-        mcTableTmp = calcParticleZe(dicSettings['wl'], dicSettings['elv'],
-                                mcTableTmp, ndgs=dicSettings['ndgsVal'],
-                                scatSet=dicSettings['scatSet'])
-
-        tmpSpecXR = getMultFrecSpec(dicSettings['wl'], dicSettings['elv'],mcTableTmp, dicSettings['velBins'],
-                                dicSettings['velCenterBin'], (heightEdge1+heightEdge0)/2,dicSettings['convolute'],dicSettings['nave'],dicSettings['noise_pow'],
-                                dicSettings['eps_diss'], dicSettings['uwind'], dicSettings['time_int'], dicSettings['theta']/2./180.*np.pi, scatSet=dicSettings['scatSet'] )
+	Returns
+	-------
+	specXR: xarray dataset with the spectra(range, vel) and KDP(range)
+	"""
 
 
-        #volume normalization
-        tmpSpecXR = tmpSpecXR/vol
-        
-        specXR = xr.merge([specXR, tmpSpecXR])
+	specXR = xr.Dataset()
+	#specXR_turb = xr.Dataset()
+	vol = dicSettings['gridBaseArea'] * dicSettings['heightRes']
+	mcTable = creatRadarCols(mcTable, dicSettings)
+	for i, heightEdge0 in enumerate(dicSettings['heightRange']):
 
-        if (dicSettings['scatSet']['mode'] == 'full') or (dicSettings['scatSet']['mode'] == 'table') or (dicSettings['scatSet']['mode'] == 'wisdom') or (dicSettings['scatSet']['mode'] == 'DDA'):
-            #calculating the integrated kdp
-            tmpKdpXR =  getIntKdp(mcTableTmp,(heightEdge1+heightEdge0)/2)
-            specXR = xr.merge([specXR, tmpKdpXR/vol])
-        #print(specXR)
-        
-    return specXR
+		heightEdge1 = heightEdge0 + dicSettings['heightRes']
+
+		print('Range: from {0} to {1}'.format(heightEdge0, heightEdge1))
+		mcTableTmp = mcTable.where((mcTable['sHeight']>heightEdge0) &
+				 					(mcTable['sHeight']<=heightEdge1),drop=True)
+		
+		if mcTableTmp.vel.any():
+			mcTableTmp = calcParticleZe(dicSettings['wl'], dicSettings['elv'],
+						       			mcTableTmp, ndgs=dicSettings['ndgsVal'],
+						        		scatSet=dicSettings['scatSet'])
+
+			tmpSpecXR = getMultFrecSpec(dicSettings['wl'], dicSettings['elv'],mcTableTmp, dicSettings['velBins'],
+						        		dicSettings['velCenterBin'], (heightEdge1+heightEdge0)/2,dicSettings['convolute'],dicSettings['nave'],dicSettings['noise_pow'],
+						        		dicSettings['eps_diss'], dicSettings['uwind'], dicSettings['time_int'], dicSettings['theta']/2./180.*np.pi, scatSet=dicSettings['scatSet'] )
+
+
+			#volume normalization
+			tmpSpecXR = tmpSpecXR/vol
+
+			specXR = xr.merge([specXR, tmpSpecXR])
+
+			if (dicSettings['scatSet']['mode'] == 'full') or (dicSettings['scatSet']['mode'] == 'table') or (dicSettings['scatSet']['mode'] == 'wisdom') or (dicSettings['scatSet']['mode'] == 'DDA'):
+			#calculating the integrated kdp
+				tmpKdpXR =  getIntKdp(mcTableTmp,(heightEdge1+heightEdge0)/2)
+				specXR = xr.merge([specXR, tmpKdpXR/vol])
+				#print(specXR)
+		else:
+			print('empty dataset at this height range')
+	return specXR
 
 def singleParticleTrajectories(dicSettings, mcTable):
     """
